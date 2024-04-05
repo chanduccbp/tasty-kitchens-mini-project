eccda6cd309193ea837b755ff053fadaa80ac0e1
chanduccbp <kumarchandrasekhar57@gmail.com>
2024-04-05 13:17:14 +0000

    tasty kitchens

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,31 @@
+import {
+  FaPinterestSquare,
+  FaInstagramSquare,
+  FaTwitter,
+  FaFacebookSquare,
+} from 'react-icons/fa'
 import './index.css'
 
-export default function Footer() {
-  return (
-    <div>
-      <h1>Footer </h1>
+const Footer = () => (
+  <div className="footer">
+    <div className="footer-title">
+      <img
+        src="https://res.cloudinary.com/dgil22y25/image/upload/v1712313837/Frame_275_phtzrz.png"
+        alt="website-footer-logo"
+        className='web-footer-logo'
+      />
+      <h1>Tasty Kitchens</h1>
     </div>
-  )
-}
+    <p className="footer-desc">
+      The only thing we are serious about is food.Contact us on
+    </p>
+    <div className="social-icons">
+      <FaPinterestSquare testid="pintrest-social-icon" />
+      <FaInstagramSquare testid="instagram-social-icon" />
+      <FaTwitter testid="twitter-social-icon" />
+      <FaFacebookSquare testid="facebook-social-icon" />
+    </div>
+  </div>
+)
+
+export default Footer
